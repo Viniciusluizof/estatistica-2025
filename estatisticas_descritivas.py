3c5acc0be3693cc0716bac87cd2bfc0c3d5c5d78
Vinicius Farias <viniciusluizof@gmail.com>
2025-08-18 16:44:12 -0300

    chore: add descriptive statistics script

---
+++ estatisticas_descritivas.py
@@ -0,0 +1,43 @@
+# %%
+
+import pandas as pd
+
+df = pd.read_csv("data/points_tmw.csv")
+df.head()
+# %%
+
+print('Estatisticas de posição para transações:')
+minimo =  df['qtdPontos'].min()
+print(f'Minimo: {minimo}')
+
+media = df['qtdPontos'].mean()
+print(f'Média: {media}')
+
+quartil_1 = df['qtdPontos'].quantile(0.25)
+print(f'1º Quartil: {quartil_1}')
+
+mediana = df['qtdPontos'].median()
+print(f'Mediana: {quartil_1}')
+
+quartil_3 = df['qtdPontos'].quantile(0.75)
+print(f'3º Quartil: {quartil_3}')
+
+maximo = df['qtdPontos'].max()
+print(f'Máximo: {maximo}')
+
+df['qtdPontos'].describe().round(2)
+# %%
+
+
+# %%
+
+print('-'*20)
+usuarios = df.groupby(['idUsuario']).agg(
+                {"idTransacao":"count",
+                "qtdPontos": "sum"}
+            ).reset_index()
+
+usuarios
+# %%
+usuarios[["idTransacao","qtdPontos"]].describe().round(2)
+# %%
